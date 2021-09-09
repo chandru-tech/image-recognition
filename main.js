419e8f2c60b31ea756c05cdb3ba5003422e918b0
chandru-tech <76948108+chandru-tech@users.noreply.github.com>
2021-09-09 16:06:20 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+Webcam.set({
+    width : 350,
+    height : 300,
+    image_format : 'png',
+    png_quality : 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src = "'+data_uri+'"/>';
+    });
+}
+
+console.log('m15 version :',m15.version);
+
+classsifier = m15.imageClassifier('https://teachablemachine.withgoogle.com/models/mHtF08mnn/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log('Model loaded !');
+}
